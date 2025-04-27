0fad9dd3f92b54a1d56b79f028c90cd0676faf1f
Max McFarlane <max.mcfarlane14@gmail.com>
2025-04-27 12:03:51 +0100

    CHORE: port

--- wsgi.py
+++ wsgi.py
@@ -25,5 +25,5 @@ def display_page(pathname):
 server = app.server
 
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)
+    # app.run(debug=True)
